731f4b1a96992d9719ef2b37432beb0b2e149f2e
unknown <brahim.ezzali@hotmail.com>
2021-02-07 23:17:56 +0100

    update MyApp

--- FlutterApp1/lib/main.dart
+++ FlutterApp1/lib/main.dart
@@ -6,19 +6,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Startup Name Generator',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+          primaryColor: Colors.teal
       ),
-      home: Text('Flutter Demo Home Page'),
+
+      home: RandomWords(),
     );
-    // This is the theme of your application.
   }
+
 }
 
 class RandomWords extends StatefulWidget {
