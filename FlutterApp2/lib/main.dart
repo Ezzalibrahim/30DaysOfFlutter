f68a68fca7602f4c5000ab1bca5f9f525d8df115
unknown <brahim.ezzali@hotmail.com>
2021-02-10 00:02:29 +0100

    add Left Drawer

--- FlutterApp2/lib/main.dart
+++ FlutterApp2/lib/main.dart
@@ -9,22 +9,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("App Title"),
-            centerTitle: true,
-            backgroundColor: Colors.cyan,
-          ),
-          // Container is like a div
-          body: ListView(
-            children: [
-              for (var i = 0; i < 20; i++)
-                ListTile(
-                    title: Text("List view $i "),
-                    onTap: () {
-                      print('');
-                    })
-            ],
-          )),
+        drawer: _myDrawer(),
+        appBar: AppBar(
+          title: Text("App Title"),
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+        ),
+        // Container is like a div
+        body: _myListView(),
+      ),
+    );
+  }
+
+  Widget _myDrawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          for (var i = 1; i < 15; i++)
+            ListTile(
+              title: Text('Storys $i'),
+              // tileColor: Colors.amberAccent,
+              onTap: () {
+                print('Story');
+              },
+              onLongPress: () {
+                print('Story on long');
+              },
+            )
+        ],
+      ),
+    );
+  }
+
+  Widget _myListView() {
+    return ListView(
+      children: [
+        for (var i = 1; i < 20; i++)
+          ListTile(
+              title: Text("List view $i "),
+              onTap: () {
+                print('');
+              })
+      ],
     );
   }
 }
